b6310cfbeaf48e4944f28d3ed9b73aaa4a1b401e
Miguel Rodriguez <angek37@outlook.com>
2017-03-09 15:14:07 -0600

    Correcciones menores
    Borre variables innecesarias

--- src/Aleatorios/main.java
+++ src/Aleatorios/main.java
@@ -2,8 +2,8 @@ package Aleatorios;
 public class main {
 	public static void main(String mr[]){
 		Calculos c = new Calculos();
-		Ran r = new Ran();
 		c.Calc();
-		//r.ran();
+//		Ran r = new Ran();
+//		r.ran();
 	}
 }
--- src/Pi/NumGen.java
+++ src/Pi/NumGen.java
@@ -4,7 +4,6 @@ import Aleatorios.*;
 
 public class NumGen {
 	public static void main(String mr[]){
-//		double p[] = {0.1,0.2,0.4,0.5,0.6,0.7,0.8,0.9,0.01,0.2};
 		Engine e = new Engine(1024);
 		MonteCarlo m = new MonteCarlo(e.getNums());
 	}
