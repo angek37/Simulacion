7821c8af5de8f1c0d9f831ab88f6f994e3d79ddc
Miguel Rodriguez <angek37@outlook.com>
2017-03-09 05:02:42 -0600

    Acomode los metodos con el constructor
    Declare los arreglos en el constructor para de esta forma distribuir las variables y arreglos como necesitemos en los métodos
    
    	modified:   bin/Pi/MonteCarlo.class
    	modified:   src/Pi/MonteCarlo.java

--- src/Pi/MonteCarlo.java
+++ src/Pi/MonteCarlo.java
@@ -3,11 +3,12 @@ package Pi;
 public class MonteCarlo {
 	public MonteCarlo(double[] r){
 		super();
-		Metodo(r);
-	}
-	public void Metodo(double[] r){
 		int l=r.length;
 		double tabla[][] = new double[2][l/2];
+		Metodo(r, tabla);
+		Print(tabla, l);
+	}
+	public void Metodo(double[] r, double tabla[][]){
 		int c = 0;
 		for(int a = 0; a<r.length;){
 			for(int b = 0; b<2; b++){
@@ -16,7 +17,11 @@ public class MonteCarlo {
 			}
 			c++;
 		}
-		for(int x = 0; x<5; x++){
+	}
+	
+	public void Print(double tabla[][], int l){
+		l=l/2;
+		for(int x = 0; x<l; x++){
 			for(int y = 0; y<tabla.length; y++){
 				System.out.print(tabla[y][x]+"\t");
 			}
