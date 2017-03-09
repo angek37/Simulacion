89a94beb777fc3d151c8e3e3ce110517f29d7db4
Miguel Rodriguez <angek37@outlook.com>
2017-03-09 17:02:13 -0600

    Agregue una clase para graficar
    Cree una clase nueva para graficar los numeros aleatorios dibuje un circulo y un rectangulo representando 1x1, modifique la clase Montecarlo para que me regresara el arreglo bidimesional con x,y para poder hacer los puntos.
    
    Changes to be committed:
    	new file:   bin/Pi/Grafica$1.class
    	new file:   bin/Pi/Grafica.class
    	modified:   bin/Pi/MonteCarlo.class
    	new file:   src/Pi/Grafica.java
    	modified:   src/Pi/MonteCarlo.java

---
+++ src/Pi/Grafica.java
@@ -0,0 +1,58 @@
+package Pi;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Aleatorios.Engine;
+
+
+public class Grafica extends JFrame {
+	int l = 4096;
+	Engine e = new Engine(l);
+	MonteCarlo m = new MonteCarlo(e.getNums());
+	double p[][] = m.getArr();
+	
+	
+    private JPanel contentPane;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Grafica frame = new Grafica();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Grafica() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        setTitle("Grafica puntos");
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        setBounds(0,0,400,400);
+    }
+    
+    public void paint (Graphics g){
+    	super.paint(g);
+    	g.setColor (Color.blue);
+        g.drawRect (20, 40, 350, 350);
+        g.drawOval (20, 40, 350, 350);
+        g.setColor(Color.red);
+        for(int a = 0; a < (l/2); a++){
+        	g.drawOval ((int)(p[0][a]*350)+20, (int)(p[1][a]*350)+40, 2, 2);
+        }
+    }
+}
--- src/Pi/MonteCarlo.java
+++ src/Pi/MonteCarlo.java
@@ -1,14 +1,16 @@
 package Pi;
 
 public class MonteCarlo {
+	double tabla[][];
 	public MonteCarlo(double[] r){
 		super();
 		int l=r.length;
-		double tabla[][] = new double[2][l/2];
+		tabla = new double[2][l/2];
 		Metodo(r, tabla);
 		Print(tabla, l);
 	}
 	public void Metodo(double[] r, double tabla[][]){
+		
 		int c = 0;
 		for(int a = 0; a<r.length;){
 			for(int b = 0; b<2; b++){
@@ -27,5 +29,8 @@ public class MonteCarlo {
 			}
 			System.out.println("\n");
 		}
+	}
+	public double[][] getArr(){
+		return tabla;
 	}
 }
