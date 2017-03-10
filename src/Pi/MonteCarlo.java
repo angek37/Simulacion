bd67f24f7107e0aa5b0f99a40c09d9f872ac631c
geras <geras-yo@hotmail.com>
2017-03-09 22:41:31 -0600

    Modificando la clase montecarlo para agregar el metodo CalculoPi
    Por medio de los arreglos de numeros aleatorios se crearon parejas de numeros para el calculo de
    x^2+y^2<1 para utilizar el valor Nci para calcular la aproximación de pi utilizando la formula pi=4*(Nci/l) y
    devolver la aproximacion de pi
    
    Changes to be committed:
    	modified:   bin/Pi/MonteCarlo.class
    	modified:   src/Pi/MonteCarlo.java

--- src/Pi/MonteCarlo.java
+++ src/Pi/MonteCarlo.java
@@ -1,13 +1,20 @@
 package Pi;
 
 public class MonteCarlo {
+	
+	
 	public MonteCarlo(double[] r){
 		super();
-		int l=r.length;
+		float Nci=0;
+		float pi=0;
+		int l=r.length;	
+		System.out.println("imprimeme l "+l);
 		double tabla[][] = new double[2][l/2];
 		Metodo(r, tabla);
-		Print(tabla, l);
+		Print(tabla, l, Nci, pi);
+		CalculoPi(tabla, l,Nci,pi);
 	}
+	
 	public void Metodo(double[] r, double tabla[][]){
 		int c = 0;
 		for(int a = 0; a<r.length;){
@@ -19,13 +26,41 @@ public class MonteCarlo {
 		}
 	}
 	
-	public void Print(double tabla[][], int l){
+	public void Print(double tabla[][], int l, float Nci, float pi){
 		l=l/2;
 		for(int x = 0; x<l; x++){
 			for(int y = 0; y<tabla.length; y++){
-				System.out.print(tabla[y][x]+"\t");
+				System.out.print(tabla[y][x]+"\t");	
 			}
 			System.out.println("\n");
 		}
+	}
+	
+	public void CalculoPi(double tabla[][], int l, float Nci, float pi){
+		l=l/2;
+		double num1=0, num2=0, sum=0;
+		int c=0;
+		for(int x = 0; x<l; x++){
+			for(int y = 0; y<tabla.length; y++){
+				switch(c){
+				case 0:
+					num1=tabla[y][x];
+					c=1;
+					break;
+				case 1:
+					num2=tabla[y][x];
+					sum=(Math.pow(num1,2))+(Math.pow(num2, 2));
+					//System.out.println("sum es="+sum);
+					c=0;
+					if(sum<1){
+						Nci++;
+					}
+					break;
+				}
+			}
+		}
+		pi=4*(Nci/l);
+		System.out.println("Nci--> "+Nci);
+		System.out.println("pi= "+pi);
 	}
 }
